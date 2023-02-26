adac8c13b973833c2417c30fdef4829582422a9e
Caolin <117780085+MofoJohnson@users.noreply.github.com>
2023-02-26 20:34:00 +0000

    Add files via upload

---
+++ drum-kit/index.js
@@ -0,0 +1,65 @@
+function getSound(key) {
+    switch(key) {
+        case "w":
+            const tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "a":
+            const tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            const tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "d":
+            const tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "j":
+            const snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case "k":
+            const crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+        case "l":
+            const kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+
+        default:
+            console.log(key);
+    }
+}
+
+function buttonAnimation(currentKey) {
+    let activeButton = document.querySelector(`.${currentKey}`);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
+
+let drumButtons = document.querySelectorAll("button");
+
+for (button of drumButtons) {
+    button.addEventListener("click", function () {
+        let buttonInnerHTML = this.innerHTML;
+        getSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    });
+
+    button.addEventListener("keydown", function (event) {
+        getSound(event.key);
+        buttonAnimation(event.key);
+    })
+}
